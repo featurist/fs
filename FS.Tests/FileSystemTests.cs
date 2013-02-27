fd1bdfd499de1afdc6c592c39eb84603057f85b2
Tim Macfarlane <timmacfarlane@gmail.com>
2013-02-27 12:44:37 +0000

    easy utils for fs copy, move, delete and create

---
+++ FS.Tests/FileSystemTests.cs
@@ -0,0 +1,176 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace FS.Tests
+{
+    [TestFixture]
+    public class FileSystemTests
+    {
+        [SetUp]
+        public void SetUp() {
+            DeleteDirectory("a");
+            DeleteDirectory("b");
+        }
+
+        private static void DeleteDirectory(string path) {
+            if (Directory.Exists(path))
+                Directory.Delete(path, true);
+        }
+
+        [Test]
+        public void CanCopyDirectory() {
+            CreateFiles(@"a\one.txt", @"a\inner\two.txt");
+
+            new FileSystem().Copy("a", "b");
+
+            AssertFileContents(@"b\one.txt", @"contents of a\one.txt");
+            AssertFileContents(@"b\inner\two.txt", @"contents of a\inner\two.txt");
+        }
+
+        [Test]
+        public void CanMoveDirectory() {
+            CreateFiles(@"a\one.txt", @"a\inner\two.txt");
+
+            new FileSystem().Move("a", "b");
+
+            AssertDirectoryNotExtant(@"a");
+            AssertFileContents(@"b\one.txt", @"contents of a\one.txt");
+            AssertFileContents(@"b\inner\two.txt", @"contents of a\inner\two.txt");
+        }
+
+        [Test]
+        public void CanCopyDirectoryExcludingSomeFiles() {
+            CreateFiles(@"a\one.bad", @"a\inner\two.txt");
+
+            new FileSystem().Copy("a", "b", fn => Path.GetExtension(fn) != ".bad");
+
+            AssertFileNotExtant(@"b\one.bad");
+            AssertFileContents(@"b\inner\two.txt", @"contents of a\inner\two.txt");
+        }
+
+        [Test]
+        public void CanCopyFile() {
+            CreateFiles(@"a\one.txt");
+
+            new FileSystem().Copy(@"a\one.txt", @"b\one.txt");
+
+            AssertFileContents(@"b\one.txt", @"contents of a\one.txt");
+        }
+
+        [Test]
+        public void CanMoveFile() {
+            CreateFiles(@"a\one.txt");
+            CreateDirectoryPath("b");
+
+            new FileSystem().Move(@"a\one.txt", @"b\one.txt");
+
+            AssertFileNotExtant(@"a\one.txt");
+            AssertFileContents(@"b\one.txt", @"contents of a\one.txt");
+        }
+
+        [Test]
+        public void ItMovesFilesAndCreatesDestinationDirectoriesIfNecessary() {
+            CreateFiles(@"a\one.txt");
+
+            new FileSystem().Move(@"a\one.txt", @"b\one.txt");
+
+            AssertFileNotExtant(@"a\one.txt");
+            AssertFileContents(@"b\one.txt", @"contents of a\one.txt");
+        }
+
+        [Test]
+        public void CanCopyAFileToDirectory() {
+            CreateFiles(@"a\one.txt");
+
+            new FileSystem().CopyToDirectory(@"a\one.txt", "b");
+            AssertFileContents(@"b\one.txt", @"contents of a\one.txt");
+        }
+
+        [Test]
+        public void CanMoveAFileToDirectory() {
+            CreateFiles(@"a\one.txt");
+
+            new FileSystem().MoveToDirectory(@"a\one.txt", "b");
+            AssertFileNotExtant(@"a\one.txt");
+            AssertFileContents(@"b\one.txt", @"contents of a\one.txt");
+        }
+
+        [Test]
+        public void CanCopyADirectoryToDirectory()
+        {
+            CreateFiles(@"a\one.txt", @"a\inner\two.txt");
+
+            new FileSystem().CopyToDirectory("a", "b");
+
+            AssertFileContents(@"b\a\one.txt", @"contents of a\one.txt");
+            AssertFileContents(@"b\a\inner\two.txt", @"contents of a\inner\two.txt");
+        }
+
+        [Test]
+        public void CanMoveADirectoryToDirectory()
+        {
+            CreateFiles(@"a\one.txt", @"a\inner\two.txt");
+
+            new FileSystem().MoveToDirectory("a", "b");
+
+            AssertDirectoryNotExtant(@"a");
+            AssertFileContents(@"b\a\one.txt", @"contents of a\one.txt");
+            AssertFileContents(@"b\a\inner\two.txt", @"contents of a\inner\two.txt");
+        }
+
+        [Test]
+        public void CanDeleteDirectory() {
+            CreateFiles(@"a\one.txt", @"a\inner\two.txt");
+
+            new FileSystem().Delete("a");
+
+            AssertDirectoryNotExtant("a");
+        }
+
+        [Test]
+        public void CanDeleteFile() {
+            CreateFiles(@"a\one.txt");
+
+            new FileSystem().Delete(@"a\one.txt");
+
+            AssertFileNotExtant(@"a\one.txt");
+        }
+
+        [Test]
+        public void DoesntGoCrazyWhenFileDoenstExistForDeleting() {
+            new FileSystem().Delete(@"a\one.txt");
+
+            AssertFileNotExtant(@"a\one.txt");
+        }
+
+        private static void AssertFileNotExtant(string path) {
+            Assert.False(File.Exists(path), "expected {0} not to exist", path);
+        }
+
+        private static void AssertDirectoryNotExtant(string path) {
+            Assert.False(Directory.Exists(path), "expected {0} not to exist", path);
+        }
+
+        private void CreateFiles(params string[] paths) {
+            foreach (var path in paths) {
+                new FileSystem().CreateDirectory(Path.GetDirectoryName(path));
+                File.WriteAllText(path, "contents of " + path);
+            }
+        }
+
+        private void CreateDirectoryPath(string path) {
+            if (path != string.Empty && !Directory.Exists(path)) {
+                if (File.Exists(path)) {
+                    File.Delete(path);
+                }
+                CreateDirectoryPath(Path.GetDirectoryName(path));
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        private void AssertFileContents(string path, string contents) {
+            Assert.True(File.Exists(path), "expected {0} to exist", path);
+            Assert.That(File.ReadAllText(path), Is.EqualTo(contents));
+        }
+    }
+}
---
+++ FS/FileSystem.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+
+namespace FS
+{
+    public class FileSystem
+    {
+        public void Copy(string from, string to, Predicate<string> copyFile = null) {
+            if (Directory.Exists(from)) {
+                CopyDirectoryContents(from, from, to, copyFile ?? (fn => true));
+            } else {
+                CopyFile(from, to);
+            }
+        }
+
+        public void CopyToDirectory(string from, string toDirectory, Predicate<string> copyFile = null)
+        {
+            Copy(from, Path.Combine(toDirectory, Path.GetFileName(from)), copyFile);
+        }
+
+        public void Delete(string path) {
+            if (Directory.Exists(path)) {
+                Directory.Delete(path, true);
+            } else if (File.Exists(path)) {
+                File.Delete(path);
+            }
+        }
+
+        public void Move(string from, string to) {
+            CreateDirectory(Path.GetDirectoryName(to));
+            if (Directory.Exists(from)) {
+                Directory.Move(from, to);
+            } else if (File.Exists(from)) {
+                File.Move(from, to);
+            }
+        }
+
+        public void MoveToDirectory(string from, string toDirectory) {
+            Move(from, Path.Combine(toDirectory, Path.GetFileName(from)));
+        }
+
+        public void CreateDirectory(string path) {
+            if (path != string.Empty && !Directory.Exists(path)) {
+                if (File.Exists(path)) {
+                    File.Delete(path);
+                }
+                CreateDirectory(Path.GetDirectoryName(path));
+                Directory.CreateDirectory(path);
+            }
+        }
+
+        private void CopyFile(string from, string to) {
+            var toDirectory = Path.GetDirectoryName(to);
+            if (toDirectory != String.Empty && !Directory.Exists(toDirectory))
+            {
+                Directory.CreateDirectory(toDirectory);
+            }
+
+            File.Copy(@from, to);
+        }
+
+        private static void CopyDirectoryContents(string originalFrom, string from, string to, Predicate<string> copyFile)
+        {
+            if (!Directory.Exists(to))
+            {
+                Directory.CreateDirectory(to);
+            }
+
+            foreach (var file in Directory.GetFiles(@from))
+            {
+                var destFilename = Path.Combine(to, Path.GetFileName(file));
+
+                if (copyFile(file.Substring(originalFrom.Length + 1)))
+                {
+                    File.Copy(file, destFilename);
+                }
+            }
+
+            foreach (var subdir in Directory.GetDirectories(@from))
+            {
+                if (copyFile(subdir.Substring(originalFrom.Length + 1) + @"\"))
+                {
+                    CopyDirectoryContents(originalFrom, subdir, Path.Combine(to, Path.GetFileName(subdir)),
+                        copyFile);
+                }
+            }
+        }
+    }
+}
